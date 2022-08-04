259aa90d2c6e8ef267126176a3a1b31f50134d9b
Oghenetega Esedere <arcteggzz@gmail.com>
2022-08-04 20:32:51 +0100

    initial setup

---
+++ src/components/Controls.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-3xl font-bold underline">
+    Tega
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+</style>
